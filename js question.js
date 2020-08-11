7dc526efc808611a8594fd81099b6eb6933fbe41
hateem786 <64477516+hateem786@users.noreply.github.com>
2020-08-11 06:37:27 -0500

    Add files via upload

---
+++ js question.js
@@ -0,0 +1,30 @@
+
+// Arrray of object
+const quiz = [
+    {
+        q: 'Q. The World’s oldest known city is:',
+        options:['None of these','Idlib','Aleppo','Damascus'],
+        answer:3
+    },
+    {
+    q: ' Q. Where is the World’s longest sea bridge ?',
+    options:['Japan','Russia','China','None of These'],
+    answer:2
+},
+{
+    q:  "Q. How many divisions int the Punjab?",
+    options:['6','3','9','None of These'],
+    answer:2
+},
+{
+    q: 'Q. The world’s largest island is:',
+    options:['Greenland','Finland','Indonesia','United Kingdom'],
+    answer:0
+},
+{
+    q:'Q. The first country to give a robot citizenship:',
+    options:['UAE','Saudi Arabia','Qatar','USA'],
+    answer:1
+}
+
+]
